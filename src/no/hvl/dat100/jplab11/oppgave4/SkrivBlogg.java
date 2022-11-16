ab905119b51c7192145696117243cfe8024e860e
linnv <linnv@LAPTOP-ER7PV09R>
2022-11-16 18:59:18 +0100

    Oppgave 4

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -10,6 +10,19 @@ public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		PrintWriter skriver = null;
+		try {
+			skriver = new PrintWriter(mappe + filnavn);
+			skriver.print(samling.toString());
+			return true;
+		}
+		catch (FileNotFoundException error) {
+			System.out.print("error");
+			return false;
+		}
+		finally {
+			skriver.close();
+		}
+
 	}
 }
